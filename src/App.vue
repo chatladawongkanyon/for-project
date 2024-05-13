c8414f302544df2615e8e07f90695244dec053c2
65030034 <144875017+65030034@users.noreply.github.com>
2024-05-13 10:43:56 +0700

    init commit

---
+++ src/App.vue
@@ -0,0 +1,100 @@
+<script setup lang="ts">
+import mqtt from "mqtt";
+import { ref } from "vue";
+
+const client = mqtt.connect('wss://test.mosquitto.org:8081');
+
+const test1led1 = ref(false);
+const test1led2 = ref(false);
+const test2led1 = ref(false);
+const test2led2 = ref(false);
+
+client.subscribe("TEST1/LED1");
+client.subscribe("TEST1/LED2");
+client.subscribe("TEST2/LED1");
+client.subscribe("TEST2/LED2");
+
+client.on("message", (topic, payload) => {
+  if (topic === 'TEST1/LED1') {
+    test1led1.value = payload.toString() === 'ON';
+  } else if (topic === 'TEST1/LED2') {
+    test1led2.value = payload.toString() === 'ON';
+  } else if (topic === 'TEST2/LED1') {
+    test2led1.value = payload.toString() === 'ON';
+  } else if (topic === 'TEST2/LED2') {
+    test2led2.value = payload.toString() === 'ON';
+  }
+
+  console.log(topic, payload.toString());
+});
+
+function ledToggle(topic: string, isChecked: boolean) {
+  const message = isChecked ? "ON" : "OFF";
+  client.publish(topic, message);
+  console.log(`Toggle switch: ${topic} - ${message}`);
+}
+</script>
+
+<template>
+  <div class="flex justify-center flex-row gap-10 pt-40">
+    <div class="border card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img src="https://cdn.coingaming.io/casino-hub88/PGsoft/fortune-ox-thumbnail.jpg" alt="Shoes" />
+      </figure>
+      <div class="card-body">
+        <div class="flex justify-center">
+          <h2 class="card-title">
+            TEST1
+          </h2>
+        </div>
+        <div class="flex flex-col pl-14">
+          <div class="form-control w-52">
+            <label class="cursor-pointer label">
+              <span class="label-text">LED1</span>
+              <input type="checkbox" v-model="test1led1" class="toggle toggle-accent" @change="ledToggle('TEST1/LED1', ($event.target as any).checked)" unchecked />
+            </label>
+          </div>
+          <div class="form-control w-52">
+            <label class="cursor-pointer label">
+              <span class="label-text">LED2</span>
+              <input type="checkbox" v-model="test1led2" class="toggle toggle-accent" @change="ledToggle('TEST1/LED2', ($event.target as any).checked)" unchecked />
+            </label>
+          </div>
+        </div>
+        <div class="card-actions justify-end"></div>
+      </div>
+    </div>
+
+    <div class="border card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img src="https://cdn.hub88.io/pgsoft/pgs_fortunemouse.jpg" alt="Shoes" />
+      </figure>
+      <div class="card-body">
+        <div class="flex justify-center">
+          <h2 class="card-title">
+            TEST2
+          </h2>
+        </div>
+        <div class="flex flex-col pl-14">
+          <div class="form-control w-52">
+            <label class="cursor-pointer label">
+              <span class="label-text">LED1</span>
+              <input type="checkbox" v-model="test2led1" class="toggle toggle-accent" @change="ledToggle('TEST2/LED1', ($event.target as any).checked)" unchecked />
+            </label>
+          </div>
+          <div class="form-control w-52">
+            <label class="cursor-pointer label">
+              <span class="label-text">LED2</span>
+              <input type="checkbox" v-model="test2led2" class="toggle toggle-accent" @change="ledToggle('TEST2/LED2', ($event.target as any).checked)" unchecked />
+            </label>
+          </div>
+        </div>
+        <div class="card-actions justify-end"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/* Omit the logo styles as they are not relevant to the checkbox functionality */
+</style>
